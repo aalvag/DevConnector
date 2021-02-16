7dae70a40d862b9aecec6ca3c22b180ee29656a9
Alejandro David Alvarez <48391594+aalvag@users.noreply.github.com>
2021-02-16 16:33:15 -0300

    Test Routes

--- server.js
+++ server.js
@@ -7,4 +7,10 @@ const connectDB = require("./config/db");
 connectDB();
 
 app.get("/", (req, res) => res.send("API Running"));
+
+// Define Routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}`));
